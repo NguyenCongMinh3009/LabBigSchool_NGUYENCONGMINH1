9b4ce6770d77043ab0d35bfbd91b1868833a612c
congminh3009@gmail.com <congminh3009@gmail.com>
2022-06-09 23:05:36 +0700

    Add project files.

---
+++ LabBigSchool_NguyenCongMinh/ViewModels/CourseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using LabBigSchool_NguyenCongMinh.Models;
+
+namespace LabBigSchool_NguyenCongMinh.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+
+        [Required]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
+        public string Time { get; set; }
+
+        [Required]
+        public byte Category { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
